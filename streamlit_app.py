175c736dd861b109391bf4b363a4bba3a3b094d1
mosabek <80618355+mosabek@users.noreply.github.com>
2021-07-19 17:40:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,174 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@mosabek
+matthewbrems /
+streamlit-bccd
+forked from roboflow-ai/streamlit-bccd
+
+0
+6
+
+    6
+
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+
+    Insights
+
+streamlit-bccd/streamlit_app.py /
+@matthewbrems
+matthewbrems tweaked link
+Latest commit 423f5dc on Feb 25
+History
+1 contributor
+126 lines (99 sloc) 3.51 KB
 import streamlit as st
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+##########
+##### Set up sidebar.
+##########
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Add in location to select image.
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/)')
+
+## Add in sliders.
+confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
+overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+image = Image.open('./images/roboflow_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+image = Image.open('./images/streamlit_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
 
-    points_per_turn = total_points / num_turns
+##########
+##### Set up main app.
+##########
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+## Title.
+st.write('# Blood Cell Count Object Detection')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+## Pull in default image or user-selected image.
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
+else:
+    # User-selected image.
+    image = Image.open(uploaded_file)
+
+## Subtitle.
+st.write('### Inferenced Image')
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Base 64 encode.
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')
+
+## Construct the URL to retrieve image.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ',
+    '&format=image',
+    f'&overlap={overlap_threshold * 100}',
+    f'&confidence={confidence_threshold * 100}',
+    '&stroke=2',
+    '&labels=True'
+])
+
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
+
+image = Image.open(BytesIO(r.content))
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Display image.
+st.image(image,
+         use_column_width=True)
+
+## Construct the URL to retrieve JSON.
+upload_url = ''.join([
+    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
+    '?access_token=vbIBKNgIXqAQ'
+])
+
+## POST to the API.
+r = requests.post(upload_url,
+                  data=img_str,
+                  headers={
+    'Content-Type': 'application/x-www-form-urlencoded'
+})
+
+## Save the JSON.
+output_dict = r.json()
+
+## Generate list of confidences.
+confidences = [box['confidence'] for box in output_dict['predictions']]
+
+## Summary statistics section in main app.
+st.write('### Summary Statistics')
+st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
+st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
+
+## Histogram in main app.
+st.write('### Histogram of Confidence Levels')
+fig, ax = plt.subplots()
+ax.hist(confidences, bins=10, range=(0.0,1.0))
+st.pyplot(fig)
+
+## Display the JSON in main app.
+st.write('### JSON Output')
+st.write(r.json())
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+Loading complete
